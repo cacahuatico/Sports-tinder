eba7cd7dde50aee0f2e496db6b401ad375e13251
rui-costa <ruifcosta96@gmail.com>
2021-04-09 10:16:56 +0000

    add friends

--- src/main/java/org/academiadecodigo/cachealots/sports/controller/UserController.java
+++ src/main/java/org/academiadecodigo/cachealots/sports/controller/UserController.java
@@ -26,35 +26,36 @@ import java.util.stream.Collectors;
 public class UserController {
 
 
-        private UserServiceImpl userService;
-        private AuthServiceImpl authService;
+    private UserServiceImpl userService;
+    private AuthServiceImpl authService;
 
-        /**
-         * Sets the customer service
-         *
-         * @param userService the customer service to set
-         */
-        @Autowired
-        public void setUserService(UserServiceImpl userService) {
-            this.userService = userService;
-        }
+    /**
+     * Sets the customer service
+     *
+     * @param userService the customer service to set
+     */
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
     @Autowired
     public void setAuthService(AuthServiceImpl authService) {
         this.authService = authService;
     }
 
     /**
-         * Retrieves a representation of the list of customers
-         *
-         * @return the response entity
-         */
-        @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
-        public ResponseEntity<List<User>> listCustomers() {
+     * Retrieves a representation of the list of customers
+     *
+     * @return the response entity
+     */
+    @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
+    public ResponseEntity<List<User>> listCustomers() {
 
-                List<User> userList = userService.list();
+        List<User> userList = userService.list();
 
-            return new ResponseEntity<>(userList, HttpStatus.OK);
-        }
+        return new ResponseEntity<>(userList, HttpStatus.OK);
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = "/friends/{id}")
     public ResponseEntity<List<User>> listFriends(@PathVariable Integer id) {
@@ -65,45 +66,46 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = {"/access"})
-    public ResponseEntity authUser(@Valid @RequestBody User user, BindingResult bindingResult ) {
+    public ResponseEntity authUser(@Valid @RequestBody User user, BindingResult bindingResult) {
 
 
-         User user2 = null;
+        User user2 = null;
 
-        if(authService.authenticate(user.getEmail(),user.getPassword())){
+        if (authService.authenticate(user.getEmail(), user.getPassword())) {
             user2 = userService.get(user.getEmail());
-
+            return new ResponseEntity<>(user2, HttpStatus.OK);
         }
 
 
-        return new ResponseEntity<>(user2, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-        /**
-         * Adds a customer
-         *
-         * @param user          the user
-         * @return the response entity
-         */
-        @RequestMapping(method = RequestMethod.POST, path = {"/", ""})
-        public ResponseEntity<User> addCustomer(@Valid @RequestBody User user, BindingResult bindingResult ) {
-            List<User> userList = userService.list().stream().filter(user1 -> user1.getEmail().equals(user.getEmail())).collect(Collectors.toList());
+    /**
+     * Adds a customer
+     *
+     * @param user the user
+     * @return the response entity
+     */
+    @RequestMapping(method = RequestMethod.POST, path = {"/", ""})
+    public ResponseEntity<User> addCustomer(@Valid @RequestBody User user, BindingResult bindingResult) {
+        List<User> userList = userService.list().stream().filter(user1 -> user1.getEmail().equals(user.getEmail())).collect(Collectors.toList());
 
 
-            if (bindingResult.hasErrors() || user.getId() != null || !userList.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-
-
-                userService.add(user);
-              User user1 = userService.get(user.getEmail());
-
-                return new ResponseEntity<>(user1, HttpStatus.CREATED);
+        if (bindingResult.hasErrors() || user.getId() != null || !userList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-    @RequestMapping(method = RequestMethod.POST, path = "/many")
-    public ResponseEntity<String> addManyCustomer(@Valid @RequestBody List<User> user, BindingResult bindingResult ) {
 
-        for (User user1:user) {
+
+        userService.add(user);
+        User user1 = userService.get(user.getEmail());
+
+        return new ResponseEntity<>(user1, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/many")
+    public ResponseEntity<String> addManyCustomer(@Valid @RequestBody List<User> user, BindingResult bindingResult) {
+
+        for (User user1 : user) {
             List<User> userList = userService.list().stream().filter(user2 -> user2.getEmail().equals(user1.getEmail())).collect(Collectors.toList());
 
 
@@ -121,17 +123,17 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/addFriend/{cid}")
-    public ResponseEntity addFriend(@RequestBody User user,@PathVariable Integer cid) {
+    public ResponseEntity addFriend(@RequestBody User user, @PathVariable Integer cid) {
 
-        userService.addFriend(user.getId(),cid);
+        userService.addFriend(user.getId(), cid);
 
-        return new ResponseEntity<>("You have followed "+ userService.getById(cid).getFirstName(), HttpStatus.CREATED);
+        return new ResponseEntity<>("You have followed " + userService.getById(cid).getFirstName(), HttpStatus.CREATED);
     }
 
 
     @RequestMapping(method = RequestMethod.POST, path = "/addsport")
     public ResponseEntity addSports(@RequestBody String sports) {
-        return new ResponseEntity<>("You have followed "+ authService.getAccessingUser().getFirstName(), HttpStatus.CREATED);
+        return new ResponseEntity<>("You have followed " + authService.getAccessingUser().getFirstName(), HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/sports")
